30b1291398c822069fd64a10a16d8bcc327d0c58
dayyeeet <david@cappell.net>
2024-10-18 12:07:21 +0200

    feat: initialize project and raylib

---
+++ Game/Game.cs
@@ -0,0 +1,17 @@
+﻿namespace Game;
+
+using static Raylib_cs.Raylib;
+
+class Game
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    public static void Main()
+    {
+        // Initialization:
+
+        InitWindow(screenWidth, screenHeight, "Team SkEPsis - Game");
+        //TODO: Game Loop
+    }
+}
