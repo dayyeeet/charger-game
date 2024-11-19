171d90581cb2f413dcdcd8b5e10d71e06fc3926e
jostju97174 <jostju97174@th-nuernberg.de>
2024-11-19 18:35:42 +0100

    #30 refactor: remove texture due to Bug

--- Game/SpoonItem.cs
+++ Game/SpoonItem.cs
@@ -5,11 +5,9 @@ namespace Game;
 public class SpoonItem() : Item("spoon")
 {
 
-    private readonly Texture2D _spoony = Raylib.LoadTexture("resources/new-spoon.jpeg"); 
-    
     public override void Draw()
     {
-        //Raylib.DrawCircle((int)Position.X, (int)Position.Y, 10f, Color.Beige);
-        Raylib.DrawTexture(_spoony,(int)Position.X , (int)Position.Y, Color.Beige);
+        Raylib.DrawCircle((int)Position.X, (int)Position.Y, 10f, Color.Beige);
+
     }
 }
