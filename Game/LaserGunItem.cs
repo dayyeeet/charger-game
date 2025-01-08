abb7feffd4ef313fe2ca4f92f1bc13c60ad84d68
jostju97174 <jostju97174@th-nuernberg.de>
2025-01-08 11:27:04 +0100

    #68 feat: lasergun texture

--- Game/LaserGunItem.cs
+++ Game/LaserGunItem.cs
@@ -5,7 +5,7 @@ namespace Game;
 
 public class LaserGunItem() : GunItem("laser", new LaserGun())
 {
-    private readonly Lazy<Texture2D> _texture = new (EmbeddedTexture.LoadTexture("Game.plasma-gun.png")!.Value);
+    private readonly Lazy<Texture2D> _texture = new (EmbeddedTexture.LoadTexture("Game.lasergun.png")!.Value);
     public override Texture2D Texture => _texture.Value;
     public override void Draw()
     {
