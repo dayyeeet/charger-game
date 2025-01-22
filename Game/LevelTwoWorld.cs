ca03305d824730a7aa771a69a23345ed9262d039
jostju97174 <jostju97174@th-nuernberg.de>
2025-01-22 17:24:39 +0100

    #106 feat: tank two

--- Game/LevelTwoWorld.cs
+++ Game/LevelTwoWorld.cs
@@ -18,6 +18,7 @@ public class LevelTwoWorld(int dimensionX, int dimensionY, bool shouldLoad = tru
         populator.Populate<TestEnemySpawner>(Dimension, 0.01f);
         populator.Populate<EnemyAiRoamingPoint>(Dimension, 0.05f);
         populator.Populate<PlaneDecoration>(Dimension, 0.05f);
+        populator.Populate<Tank2Decoration>(Dimension, 0.05f);
         populator.Populate<TrashDecorationWorldFeature>(Dimension, 0.01f);
         populator.Populate<TestDestroyableObject>(Dimension, 0.001f);
     }
